0bdfb8013fae123873bc51ebbc0b65049cd5b14a
muhammdfayyazansari <fayyazansari268@gmail.com>
2025-12-17 04:50:40 +0500

    remove the to= from mail to

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -98,7 +98,7 @@ export default function Contact() {
 
               toast.success('Opening email sending app...')
               // window.location.href = `mailto:support@metamorph.design?subject=${subject}&body=${body}`
-              window.location.href = `mailto:to=muhammadfayyaz3278@gmail.com?subject=${subject}&body=${body}`
+              window.location.href = `mailto:muhammadfayyaz3278@gmail.com?subject=${subject}&body=${body}`
               resetForm()
             }}
           >
